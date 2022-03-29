34f7e0a0e3c6f968bebc9b1f7e4485a7e96097b6
Kassy729 <hamtol98@gmail.com>
2022-03-29 11:16:33 +0900

    쿼리스트링 수정

--- app/Http/Controllers/TrackController.php
+++ app/Http/Controllers/TrackController.php
@@ -21,10 +21,19 @@ class TrackController extends Controller
         return json_decode($response, true);
     }
 
-    public function search($id)
+    public function search(Request $request)
     {
+        $bound1 = $request->query('bound1');
+        $bound2 = $request->query('bound2');
+        $bound3 = $request->query('bound3');
+        $bound4 = $request->query('bound4');
+        $event = $request->query('event');
+
+        //쿼리스트링을 만듦
+        $query = "bounds" . '=' . $bound1  . '&' .  "bounds" . '=' . $bound2 . '&' . "bounds" . '=' . $bound3 . '&' . "bounds" . '=' . $bound4 . '&' . "event" . '=' . $event;
         //Node에서 track_id를 리턴
-        $response = Http::get("http://13.124.24.179/api/tracks/search?$id");
+        $response = Http::get("http://13.124.24.179/api/tracks/search?$query");
+
         //JSON 문자열을 변환하여 값을 추출
         return json_decode($response, true);
     }
