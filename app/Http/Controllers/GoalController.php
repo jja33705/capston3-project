1608779a225299576f8c4c75996d266bfe0cee4e
Kassy729 <hamtol98@gmail.com>
2022-04-19 20:02:37 +0900

    수정

--- app/Http/Controllers/GoalController.php
+++ app/Http/Controllers/GoalController.php
@@ -117,12 +117,14 @@ class GoalController extends Controller
             }
         }
 
+        return $run_data;
+
         //자전거 목표 성공여부 체크
         for ($i = 0; $i < count($goal); $i++) {
             if ($data[$i]['distance'] >= $goal[$i]['goalDistance']) {
                 Goal::where('id', '=', $goal[$i]['id'])->update(['success' => true]);
             }
-            $goal[$i]['progress'] = $data[$i]['distance'] / $goal[$i]['goalDistance'] * 100;
+            $goal[$i]['progress'] = floor($data[$i]['distance'] / $goal[$i]['goalDistance'] * 100);
             if ($goal[$i]['progress'] >= 100) {
                 $goal[$i]['progress'] = 100;
             }
@@ -133,7 +135,7 @@ class GoalController extends Controller
             if ($run_data[$i]['distance'] >= $run_goal[$i]['goalDistance']) {
                 Goal::where('id', '=', $run_goal[$i]['id'])->update(['success' => true]);
             }
-            $run_goal[$i]['progress'] = $run_data[$i]['distance'] / $run_goal[$i]['goalDistance'] * 100;
+            $run_goal[$i]['progress'] = floor($run_data[$i]['distance'] / $run_goal[$i]['goalDistance'] * 100);
             if ($run_goal[$i]['progress'] >= 100) {
                 $run_goal[$i]['progress'] = 100;
             }
